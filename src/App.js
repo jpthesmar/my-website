c8609c99ec7fb67b0e89062af5c81f3636c4c2ff
Joseph Thesmar <thesmarjoseph@gmail.com>
2023-12-28 07:59:23 -0500

    first page prototype done, image and menu bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
 import React from 'react';
 import './App.css';
+import profilePic from './images/profilepic.PNG';
+
+const NavigationBar = () => {
+  return (
+    <div  className='navbar'>
+      <div className='navbar-left'> Joseph Thesmar</div>
+      <div className='navbar-right'>
+        <button>Work</button>
+        <button>About</button>
+        <button>Contact</button>
+      </div>
+    </div>
+  )
+}
+
+const MainContent = () => {
+  return (
+    <div className='main-content'>
+      <img src={profilePic} alt='Joseph Thesmar' className='profile-image'/>
+      <div className='description'>
+        <h2>Joseph Thesmar</h2>
+        <p>My Name is Joseph Thesmar...</p>
+      </div>
+    </div>
+  )
+}
 
 function App() {
   return (
-    <div className="App" >
-      <h1>Portfolio</h1>
+    <div>
+      <NavigationBar/>
+      <MainContent/>
     </div>
   );
 }
