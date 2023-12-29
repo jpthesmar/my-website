b40f337555939bc065f1b1c0bd33e8e20c9894af
Joseph Thesmar <thesmarjoseph@gmail.com>
2023-12-29 05:15:12 -0500

    finished home page, all buttons work and route to other pages whilst keeping the nav/menu bar up top, default page = home page

--- src/App.js
+++ src/App.js
@@ -1,38 +1,104 @@
-import React from 'react';
 import './App.css';
-import profilePic from './images/profilepic.PNG';
+import React, { useState } from 'react';
+import profilePic from './images/profilepic.png';
+import linkedInIcon from './images/linkedin.png';
+import githubIcon from './images/github-sign.png';
 
-const NavigationBar = () => {
+const NavigationBar = ({ setActiveSection }) => {
   return (
-    <div  className='navbar'>
-      <div className='navbar-left'> Joseph Thesmar</div>
+    <div className='navbar'>
+      <div className='navbar-left'> Joseph Thesmar's Portfolio</div>
       <div className='navbar-right'>
-        <button>Work</button>
-        <button>Skills</button>
-        <button>About</button>
-        <button>Contact</button>
+        <button onClick={() => setActiveSection('home')}>Home</button>
+        <button onClick={() => setActiveSection('work')}>Work</button>
+        <button onClick={() => setActiveSection('skills')}>Skills</button>
+        <button onClick={() => setActiveSection('about')}>About</button>
+        <button onClick={() => setActiveSection('contact')}>Contact</button>
       </div>
     </div>
-  )
-}
+  );
+};
 
-const MainContent = () => {
+const Home = () => {
   return (
     <div className='main-content'>
-      <img src={profilePic} alt='Joseph Thesmar' className='profile-image'/>
+      <img src={profilePic} alt='Joseph Thesmar' className='profile-image' />
       <div className='description'>
-        <h2>Joseph Thesmar</h2>
-        <p>My Name is Joseph Thesmar...</p>
+        <p>Welcome to my Portfolio! I am an enthusiastic software developer with a strong programming foundation which has contributed to impactful projects, and work experiences. Explore my diverse skills, from Python to Java to project management tools, and discover my commitment to excellence and innovation. </p>
+      </div>
+      <div className='icons-container'>
+        {/* LinkedIn Button*/}
+        <a href='https://www.linkedin.com/in/jpthesmar/' target='blank' rel='noopener noreferrer'>
+          <img src={linkedInIcon} alt='LinkedIn' className='icon' />
+        </a>
+
+        {/* GitHub Button*/}
+        <a href='https://github.com/jpthesmar' target='blank' rel='noopener noreferrer'>
+          <img src={githubIcon} alt='GitHub' className='icon' />
+        </a>
       </div>
     </div>
-  )
-}
+  );
+};
 
-function App() {
+const Work = () => {
   return (
     <div>
-      <NavigationBar/>
-      <MainContent/>
+      <p>Work</p>
+    </div>
+  );
+};
+
+const Skills = () => {
+  return (
+    <div>
+      <p>Skills</p>
+    </div>
+  );
+};
+
+const About = () => {
+  return (
+    <div>
+      <p>About</p>
+    </div>
+  );
+};
+
+const Contact = () => {
+  return (
+    <div>
+      <p>Contact</p>
+    </div>
+  );
+};
+
+
+
+function App() {
+  const [activeSection, setActiveSection] = useState('home');
+
+  const renderSection = () => {
+    switch (activeSection) {
+      case 'work':
+        return <Work />;
+      case 'skills':
+        return <Skills />;
+      case 'about':
+        return <About />;
+      case 'contact':
+        return <Contact />;
+      default:
+        console.warn(`Unexpected section: ${activeSection}`);
+        return <Home />;
+    };
+  };
+  
+
+  return (
+    <div>
+      <NavigationBar setActiveSection = {setActiveSection}/>
+      {renderSection()}
     </div>
   );
 }
